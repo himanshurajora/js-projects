4a33812548bee93e3b7e89184e75ae247bea93dc
Himanshu Jangid <iamahalofan@gmail.com>
2021-05-23 17:12:48 +0530

    Read More

---
+++ readmore-button/script.js
@@ -0,0 +1,7 @@
+function readToggle(event){
+    var button = event.srcElement;
+    button.innerText = button.innerText == "Read more" ? "Read less" : "Read more";
+    button.parentElement.firstElementChild.firstElementChild.innerText =  button.parentElement.firstElementChild.firstElementChild.innerText == "..." ? " " : "..."; 
+    button.parentElement.firstElementChild.lastChild.style.display = button.parentElement.firstElementChild.lastChild.style.display == "inline" ? "none" : "inline";
+"inline"
+}
